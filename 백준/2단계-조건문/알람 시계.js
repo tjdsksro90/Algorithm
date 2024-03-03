c0fb1f6b78a8ca9bccc2eaa3ef668ee3ea618e40
hojun.yoon <tjdsksro90@gmail.com>
2024-03-03 16:46:50 +0900

    백준 2단계 알람 시계

---
+++ 백준/2단계-조건문/알람 시계.js
@@ -0,0 +1,30 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  let input = line.split(' ');
+
+  let num1 = Number(input[0]);
+  let num2 = Number(input[1]);
+  
+  if (num2 < 45) {
+    num2 = 60 + (num2 - 45);
+    num1 -= 1;
+
+    if (num1 === -1) {
+      num1 = 23;
+    }
+  } else {
+    num2 -= 45;
+  }
+
+  console.log(num1 + ' ' + num2);
+}).on('close', function () {
+  process.exit();
+});
