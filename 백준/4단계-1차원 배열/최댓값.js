e7f2b5cd86b26d64048913772e8de93a12358984
hojun.yoon <tjdsksro90@gmail.com>
2024-03-07 09:18:41 +0900

    Revert "4단계 백준 최소,최대"
    This reverts commit 3d91251251982d9e6be1fb005c73695da6b1f49a.

--- 백준/4단계-1차원 배열/최댓값.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n').map(x => Number(x));
-
-let max = Math.max(...input);
-console.log(max);
-console.log(input.indexOf(max) + 1);
