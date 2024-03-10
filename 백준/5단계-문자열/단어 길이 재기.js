f5e3eef557366b34a4db9d425569c15f0b149776
hojun.yoon <tjdsksro90@gmail.com>
2024-03-10 22:48:26 +0900

    백준 5단계 단어 길이 재기

---
+++ 백준/5단계-문자열/단어 길이 재기.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+//백준 문제제출
+//const input = fs.readFileSync('dev/stdin').toString().trim();
+
+//문제풀이용
+const input = fs.readFileSync('test.txt').toString().trim();
+console.log(input.length)
