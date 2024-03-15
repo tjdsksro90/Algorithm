206b7c2bfc409fc38dc609cf86720f3ba03d23c1
hojun.yoon <tjdsksro90@gmail.com>
2024-03-15 13:08:15 +0900

    프로그래머스 예산

---
+++ 프로그래머스/Summer Winter Coding(~2018)/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+    let answer = 0;
+    d.sort((a,b)=> a-b)
+    for( let i=0; i<d.length; i++){
+        if(budget < d[i]) break;
+        
+        budget -= d[i];
+        answer += 1;
+    }
+    return answer;
+}
