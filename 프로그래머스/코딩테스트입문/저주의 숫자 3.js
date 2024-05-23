5ba7d4959b0f33efbcb0bcf2feab8d86ff9e42b2
hojun.yoon <tjdsksro90@gmail.com>
2024-05-23 10:55:23 +0900

    ✨ update: 저주의 숫자 3

---
+++ 프로그래머스/코딩테스트입문/저주의 숫자 3.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  let answer = 0;
+
+  for (let i = 1; i <= n; i++) {
+    answer++;
+    while (answer.toString().includes("3") || answer % 3 === 0) {
+      answer++;
+    }
+  }
+  return answer;
+}
