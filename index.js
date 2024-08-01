54f0d4d13a6f0b59ff3aa518eda4ef35c913790e
hojun.yoon <tjdsksro90@gmail.com>
2024-08-01 17:21:31 +0900

    ✨ update: 백준 index 업데이트

--- index.js
+++ index.js
@@ -2,24 +2,9 @@ const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "test.txt";
 let input = fs.readFileSync(file).toString().trim().split("\n");
 
-// 9506번: 약수들의 합
+// 10814번: 나이순 정렬
+// https://www.acmicpc.net/problem/10814
 
-for (let i = 0; i < input.length; i++) {
-  let number = parseInt(input[i]);
-  if (number === -1) break;
-
-  let divisors = [];
-  for (let j = 1; j < number; j++) {
-    if (number % j === 0) {
-      divisors.push(j);
-    }
-  }
-
-  let sumOfDivisors = divisors.reduce((acc, cur) => acc + cur, 0);
-
-  if (sumOfDivisors === number) {
-    console.log(`${number} = ${divisors.join(" + ")}`);
-  } else {
-    console.log(`${number} is NOT perfect.`);
-  }
-}
+const [N, ...arr] = input;
+arr.sort((a,b)=> a.split(' ')[0] - b.split(' ')[0]);
+arr.forEach((item)=> console.log(item));
