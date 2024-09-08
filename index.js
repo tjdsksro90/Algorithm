26c85b1264830d2a8afc08f90463f2301eb19700
hojun.yoon <tjdsksro90@gmail.com>
2024-09-08 15:31:24 +0900

    ✨ update: 백준 index 업데이트

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "test.txt";
 let input = fs.readFileSync(file).toString().trim().split("\n");
 
-// 10814번: 나이순 정렬
-// https://www.acmicpc.net/problem/10814
+// 1546번: 평균
+// https://www.acmicpc.net/problem/1546
 
 const [N, ...arr] = input;
-arr.sort((a,b)=> a.split(' ')[0] - b.split(' ')[0]);
-arr.forEach((item)=> console.log(item));
+const list = [...arr][0].split(" ");
+let result = 0;
+let newArr = [];
+const max = Math.max(...list);
+
+// list 중에 최댓값 고르기 = M
+for (let i = 0; i < N; i++) {
+  newArr.push((Number(list[i]) / max) * 100);
+}
+
+// console.log((30 / 30) * 100);
+for (let i = 0; i < N; i++) {
+  result += Number(newArr[i]);
+}
+
+console.log((result / N).toFixed(6));
